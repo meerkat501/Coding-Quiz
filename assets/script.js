3893b163169d561fcc5e851f2754bee150947b9b
meerkat501 <leonardosoto911@gmail.com>
2023-10-02 22:31:22 -0500

    Added highscores and restart button

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,10 @@ const timerEl = document.getElementById('timeLeft');
 const gameOverEl = document.getElementById('gameOver');
 const initialsEl = document.getElementById('initials');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const highScoreBtn = document.getElementById('highScoreBtn');
+const highScoreModal = document.getElementById('highScoreModal');
+const closeHighScoresBtn = document.getElementById('closeHighScores');
+const restartBtn = document.getElementById('restartBtn');
 
 let questions = [
     {
@@ -15,30 +19,29 @@ let questions = [
     },
 
     {
-        question: "What's the capital of France?",
-        answers: ['Berlin', 'Paris', 'Madrid', 'Rome'],
+        question: "What does HTML stand for?",
+        answers: ['Hyper Text Markup Language', 'Hyper Trainer Maker Language', 'Hyper Text Max Level', 'Hoops Test Meow Language'],
+        correctAnswer: 0
+    },
+
+    {
+        question: "In JavaScript, which keyword is used to declare a variable?",
+        answers: ['Let', 'Var', 'getElement', 'document'],
         correctAnswer: 1
     },
 
     {
-        question: "What's the capital of France?",
-        answers: ['Berlin', 'Paris', 'Madrid', 'Rome'],
-        correctAnswer: 1
+        question: "CSS stands for _______?",
+        answers: ['Creative style sheet', 'Computer Shoe Style', 'Creating Style Sheet', 'Cascading Style Sheet'],
+        correctAnswer: 3
     },
 
     {
-        question: "What's the capital of France?",
-        answers: ['Berlin', 'Paris', 'Madrid', 'Rome'],
+        question: "Which JavaScript method is used to store data in the client's local storage?",
+        answers: ['localstorage.setdata()', 'localstorage.setitem()', 'localstorage.storelocal()', 'localstorage.setlocal()'],
         correctAnswer: 1
     },
 
-    {
-        question: "What's the capital of France?",
-        answers: ['Berlin', 'Paris', 'Madrid', 'Rome'],
-        correctAnswer: 1
-    },
-
-    // ... more questions
 ];
 
 let currentQuestionIndex = 0;
@@ -57,7 +60,7 @@ function startGame() {
 function updateTimer() {
     timeLeft--;
     timerEl.textContent = timeLeft;
-
+    document.getElementById('score').textContent = "Score: " + timeLeft;
     if (timeLeft <= 0) {
         endGame();
     }
@@ -77,7 +80,7 @@ function displayQuestion() {
 
 function checkAnswer(answerIndex) {
     if (answerIndex !== questions[currentQuestionIndex].correctAnswer) {
-        timeLeft -= 10;  // penalize for wrong answer
+        timeLeft -= 25;  // penalize for wrong answer
     }
     
     currentQuestionIndex++;
@@ -103,4 +106,60 @@ function saveScore() {
     localStorage.setItem('initials', initials);
     localStorage.setItem('score', score);
     // save initials and score to local storage
+}
+
+function saveScore() {
+    const initials = initialsEl.value;
+    const score = timeLeft;
+
+    // Retrieve the current high scores from localStorage
+    const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
+    
+    highScores.push({ initials, score });
+
+    // Sort scores in descending order and keep the top 5
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+}
+
+highScoreBtn.addEventListener('click', () => {
+    displayHighScores();
+    highScoreModal.style.display = 'block';  
+});
+
+closeHighScoresBtn.addEventListener('click', () => {
+    highScoreModal.style.display = 'none';   
+});
+
+function displayHighScores() {
+    const highScores = JSON.parse(localStorage.getItem('highScores') || '[]');
+    const highScoresList = document.getElementById('highScoresList');
+    highScoresList.innerHTML = '';
+    
+    highScores.forEach(scoreEntry => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${scoreEntry.initials}: ${scoreEntry.score}`;
+        highScoresList.appendChild(listItem);
+    });
+}
+
+function endGame() {
+    clearInterval(timer);
+    questionContainer.style.display = 'none';
+    gameOverEl.style.display = 'block';
+    displayHighScores();  // Displays the high scores
+}
+
+restartBtn.addEventListener('click', () => {
+    restartGame();
+});
+
+function restartGame() {
+    currentQuestionIndex = 0;    // Reset question index
+    timeLeft = 100;              // Reset timer
+    gameOverEl.style.display = 'none';  // Hide game over screen (if shown)
+    startGame();                 // Start the game
 }
